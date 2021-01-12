6cd2b30c5b3f7b55148b56233adebf9f86421573
LuGary <larry@transcend.io>
2021-01-12 13:29:14 -0500

    Add new output function_arn

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,11 @@
 // ARN of the lambda function with the most recently built version attached.
-output arn {
+output "arn" {
   value = "${aws_lambda_function.lambda.arn}:${aws_lambda_function.lambda.version}"
 }
 
-output function_name {
+output "function_arn" {
+  value = aws_lambda_function.lambda.arn
+}
+output "function_name" {
   value = var.name
 }
