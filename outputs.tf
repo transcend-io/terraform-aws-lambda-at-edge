eaba588ce5fbcf73e1517dcd8e0a74759c261f82
Joshua Wilson <10873576+jwilson8767@users.noreply.github.com>
2021-08-12 11:43:40 -0400

    Add role name and ARN to outputs (#8)
    * Add role arn to outputs
    
    * add execution_role_name output

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "function_arn" {
 output "function_name" {
   value = var.name
 }
+
+output execution_role_name {
+  value = aws_iam_role.lambda_at_edge.name
+}
+
+output execution_role_arn {
+  value = aws_iam_role.lambda_at_edge.arn
+}
