b365835ba4b840d661626b29ec8cf7d1748fb22d
Hunter Tunnicliff <hunter.tunnicliff@transcend.io>
2026-06-11 13:20:02 -0700

    Add missing version constraints

--- versions.tf
+++ versions.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
+      version = ">= 2.0.0"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = ">= 3.11.0"
     }
   }
   required_version = ">= 0.13"
